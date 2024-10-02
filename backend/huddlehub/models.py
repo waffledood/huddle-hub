2b946fce08cf29db0cb44a70301b63a8b1ceec03
waffledood <itshaikalhere@hotmail.sg>
2024-10-02 22:58:58 +0800

    Define & register RSVP model

--- backend/huddlehub/models.py
+++ backend/huddlehub/models.py
@@ -9,3 +9,12 @@ class Event(models.Model):
 
     def __str__(self):
         return f"Event ({self.id}): {self.title} organized by {self.organizer.get_username()}"
+
+
+class RSVP(models.Model):
+    participant = models.ForeignKey(to=User, on_delete=models.CASCADE)
+    event = models.ForeignKey(to=Event, on_delete=models.CASCADE, related_name="RSVPs")
+    created = models.DateField(auto_now_add=True)
+
+    def __str__(self) -> str:
+        return f"RSVP ({self.id}): {self.participant.get_username()} attending Event {self.event.id}"
