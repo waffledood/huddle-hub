3fd45f618dd3cb1ccb4b8755b3ef32e14503b314
waffledood <itshaikalhere@hotmail.sg>
2024-10-02 22:43:04 +0800

    Define & register Event model

--- backend/huddlehub/models.py
+++ backend/huddlehub/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Event(models.Model):
+    title = models.CharField(max_length=256, default="New Event")
+    organizer = models.ForeignKey(to=User, on_delete=models.CASCADE)
+    created = models.DateField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Event ({self.id}): {self.title} organized by {self.organizer.get_username()}"
