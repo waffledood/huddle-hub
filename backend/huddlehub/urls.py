320f25dbf8955a00bedf9900955db4ae75dc8f0f
waffledood <itshaikalhere@hotmail.sg>
2024-10-03 22:57:26 +0800

    Define create, login & logout URLs & views

--- backend/huddlehub/urls.py
+++ backend/huddlehub/urls.py
@@ -2,4 +2,9 @@ from django.urls import path
 
 from . import views
 
-urlpatterns = [path(route="home/", view=views.main, name="main")]
+urlpatterns = [
+    path(route="home/", view=views.index, name="index"),
+    path(route="create/", view=views.create, name="create"),
+    path(route="login/", view=views.login, name="create"),
+    path(route="logout/", view=views.logout, name="create"),
+]
--- backend/huddlehub/views.py
+++ backend/huddlehub/views.py
@@ -1,6 +1,40 @@
-from django.http import HttpResponse
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
+from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
+from django.urls import reverse
 
 
-def main(request):
+def index(request):
     return render(request=request, template_name="index.html", context={})
+
+
+@login_required(login_url="/login/")
+def create(request):
+    pass
+
+
+def login(request):
+    if request.method == "POST":
+        # Attempt to sign user in
+        username = request.POST["username"]
+        password = request.POST["password"]
+        user = authenticate(request, username=username, password=password)
+
+        # Check if authentication is successful
+        if user is not None:
+            login(request, user)
+            return HttpResponseRedirect(reverse("main"))
+        else:
+            return render(
+                request,
+                "login.html",
+                {"message": "Invalid credentials."},
+            )
+    else:
+        return render(request, "login.html")
+
+
+def logout(request):
+    logout(request)
+    return HttpResponseRedirect(reverse("main"))
