dcdbda11f6c49d2a82f22e142270d9ec95bab3a6
waffledood <itshaikalhere@hotmail.sg>
2024-10-04 23:49:44 +0800

    Update name of login & logout URLs

--- backend/huddlehub/urls.py
+++ backend/huddlehub/urls.py
@@ -5,6 +5,6 @@ from . import views
 urlpatterns = [
     path(route="home/", view=views.index, name="index"),
     path(route="create/", view=views.create, name="create"),
-    path(route="login/", view=views.login, name="create"),
-    path(route="logout/", view=views.logout, name="create"),
+    path(route="login/", view=views.login, name="login"),
+    path(route="logout/", view=views.logout, name="logout"),
 ]
