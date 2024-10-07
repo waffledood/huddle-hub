acb786ee2cc83e44f31ace5ca03e706a0ed33cba
waffledood <itshaikalhere@hotmail.sg>
2024-10-07 17:00:14 +0800

    Define manager for User model

--- backend/huddlehub/models.py
+++ backend/huddlehub/models.py
@@ -1,8 +1,9 @@
 from django.db import models
-from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import AbstractUser, UserManager
 
 
 class User(AbstractUser):
+    objects = UserManager()
     pass
 
 
