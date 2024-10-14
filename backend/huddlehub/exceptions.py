061255e719188fb7bbe1596e36ef08fe2b558168
waffledood <itshaikalhere@hotmail.sg>
2024-10-14 21:59:34 +0800

    Refactor InvalidUserAction class name

--- backend/huddlehub/exceptions.py
+++ backend/huddlehub/exceptions.py
@@ -1,3 +1,3 @@
-class InvalidUserAction(Exception):
+class InvalidUserActionException(Exception):
     def __init__(self, message="Organizer can't RSVP for their own Event."):
         super().__init__(message)
